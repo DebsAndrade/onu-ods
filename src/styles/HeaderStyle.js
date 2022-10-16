2aca22963787b3faeff84890b931ee2d9f223651
deboraandrade <deboraellenandrade@gmail.com>
2022-10-16 19:37:13 +0100

    Muda a largura da header

--- src/styles/HeaderStyle.js
+++ src/styles/HeaderStyle.js
@@ -1,7 +1,7 @@
 import styled  from "styled-components";
 
 export const ContainerHeader = styled.header`
-    width: 100vw;
+    width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
